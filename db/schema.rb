4b56b20544524977a57e825be930db384b27abe6
nishinotakuma <takuuma.717@gmail.com>
2017-04-26 17:18:31 +0900

    skillモデル、コントローラ作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170426080927) do
+ActiveRecord::Schema.define(version: 20170426081634) do
+
+  create_table "skills", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
